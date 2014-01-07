a19fc2944e39aef4e3eb921020c31d984c9b62cc
Victor <vcastro@itquality.com>
2014-01-07 14:16:50 -0200

    Criando estrutura basica do SGR

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SGR::Application.config.secret_key_base = '5d42366455d2303512dd8a7de0d6d1a6338b534d64281b62cae3aafc6c8db81ccd3fb12699602a090a78c16007d644d2ab7b10ff0f4a5ff6f83e6c11de0142ea'
