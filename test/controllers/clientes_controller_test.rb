8c32454771914a97ab3afdccc7535abc1e7af846
Victor <vcastro@itquality.com>
2014-01-29 15:50:39 -0200

    Implementado o testes dos controllers clientes e funcionarios

---
+++ test/controllers/clientes_controller_test.rb
@@ -0,0 +1,18 @@
+require 'test_helper'
+
+class ClientesControllerTest < ActionController::TestCase
+  setup do
+    @cliente_salvar = Cliente.new nome: 'Mundo E', telefone: '74952926', cartao: cartoes(:cartao_jose)
+    @cliente = clientes(:victor)
+    @cliente_grid = {id: 4, nome: 'Miguel Palomo de Castro', numero_cartao: '1111111115', telefone: '74952926', cep: '23550360'}
+  end
+
+  test 'get cadastros' do
+    get :cadastros
+    assert_response :success
+  end
+
+  test 'testar grid cadastros' do
+    validate_palom_grid :ajax_cadastros_grid, [:nome, :numero_cartao], [:telefone, :cep], @cliente_grid
+  end
+end
