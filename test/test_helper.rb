0e19b97f20261599639a00b808c9860000844cef
Victor <vcastro@itquality.com>
2014-01-29 15:50:04 -0200

    Implementado o sistema de tete para a jqgrid

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,9 @@
 ENV["RAILS_ENV"] ||= "test"
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require 'pry'
+require 'better_errors'
+require 'meta_request'
 
 class ActiveSupport::TestCase
   ActiveRecord::Migration.check_pending!
@@ -12,23 +15,81 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
-  def exijir_presenca(model, *campos)
-    validar(model, *campos) { assert_not model.save }
+  def required(model, *fields)
+    validate(model, *fields) { assert_not model.save }
   end
 
-  def nao_exijir_presenca(model, *campos)
-    validar(model, *campos) { assert model.save }
+  def not_required(model, *fields)
+    validate(model, *fields) { assert model.save }
   end
 
-  def validar(model, *campos)
-    campos.each do |campo|
-      model[campo] = nil
-      campos.each do |campo|
-        model[campo] = 123 unless model[campo] == nil
+  def validate_palom_grid(url, columns_require, columns_not_require, data)
+    opers = ['add', 'edit']
+
+    #REQUIRED TEST
+    opers.each do |oper|
+      test_columns = get_id(data)
+      validate_opers(oper, url, test_columns, columns_require, data) do |columns, column|
+
+        if column != columns.last
+          assert_response :internal_server_error, "REQUIRE Column: #{column} Operation: #{oper}, expect a error inside grid"
+        else
+          assert_response :ok, "REQUIRE Column: #{column} Operation: #{oper}, expect a ok inside grid"
+        end
+      end
+    end
+
+    #DELETE TEST
+    test_columns = get_id(data)
+    get url, test_columns.merge({oper: 'del'})
+    assert_response :ok, 'Operation: del, expect a ok inside grid'
+
+
+    #LOAD REQUIRED IF EXIST SOME
+    test_columns = get_id(data)
+
+    columns_require.each do |column|
+      test_columns[column] = data[column]
+    end
+
+    #NOT REQUIRED TEST
+    opers.each do |oper|
+      validate_opers(oper, url, test_columns, columns_not_require, data) do |columns, column|
+        assert_response :ok, "NOT REQUIRE Column: #{column} Operation: #{oper}, expect a ok inside grid"
+        case oper
+          when 'add'
+            #DELETE AND CONTINUE TESTING
+            binding.pry
+            if column != columns.last
+              get url, test_columns.merge({oper: 'del'})
+              assert_response :ok, 'Operation: del, expect a ok inside grid'
+            end
+        end
+      end
+    end
+  end
+
+  def validate(model, *fields)
+    fields.each do |field|
+      model[field] = nil
+      fields.each do |field|
+        model[field] = 123 unless model[field] == nil
         yield
       end
 
     end
   end
-end
 
+  def validate_opers(oper, url, test_columns, columns, data)
+    columns.each do |column|
+      test_columns[column] = data[column]
+      get url, test_columns.merge({oper: oper})
+      yield columns, column if block_given?
+    end
+  end
+
+  def get_id(data)
+    test_columns = Hash.new
+    test_columns.merge({id: data[:id]})
+  end
+end
