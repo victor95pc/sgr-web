41b205de01a1ae083d849a6c72f0075b72848607
victor95 <victor95@victorpalomocastro@gmail.com>
2014-01-30 20:59:15 -0200

    Implementado helper lista_menu_superior que é responsavel por listar os menus no layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,20 @@
+#encoding: utf-8
 module ApplicationHelper
+
+  def listar_menu_superior
+    menu_superior = {
+        'Clientes' => clientes_cadastros_path,
+        'Funcionários' => funcionarios_cadastros_path
+    }
+
+    menu_superior.each do |nome_menus, link_menus|
+      if request.path == link_menus
+        ativo = 'ativo'
+      else
+        ativo = nil
+      end
+      yield nome_menus, link_menus, ativo
+    end
+  end
+
 end
