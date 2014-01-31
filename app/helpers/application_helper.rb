2952d71a87c2e6a92db25c55cc9d170dd1348432
victor95 <victor95@victorpalomocastro@gmail.com>
2014-01-31 00:54:34 -0200

    Listamento dos menus superior foi refatorado

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,12 +3,12 @@ module ApplicationHelper
 
   def listar_menu_superior
     menu_superior = {
-        'Clientes' => clientes_cadastros_path,
-        'Funcionários' => funcionarios_cadastros_path
+        'Clientes' => clientes_path,
+        'Funcionários' => funcionarios_path
     }
 
     menu_superior.each do |nome_menus, link_menus|
-      if request.path == link_menus
+      if request.path.start_with? link_menus
         ativo = 'ativo'
       else
         ativo = nil
