4f0d84dd950a4b808ec32bd41a4df4ca4685b757
victor95 <victor95@victorpalomocastro@gmail.com>
2014-01-31 00:58:50 -0200

    Implementado testes para a palom grid da aba funcionarios

--- test/controllers/funcionarios_controller_test.rb
+++ test/controllers/funcionarios_controller_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class FuncionariosControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @funcionario_grid = {id: 2, nome: 'Victor Palomo de Castro', cargo: 'Dono do Mundo Socialista', salario: 0}
+  end
+
+  test 'get cadastros' do
+    get :cadastros
+    assert_response :success
+  end
+
+  test 'testar grid cadastros' do
+    validate_palom_grid :ajax_cadastros_grid, [:nome, :cargo, :salario], [], @funcionario_grid
+  end
 end
