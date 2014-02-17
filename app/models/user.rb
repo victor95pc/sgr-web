a022ae84c234dad50771c50bd2909530dd2aab3d
Victor <vcastro@itquality.com>
2014-02-17 14:12:35 -0300

    Todas as View do Devise implementado

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :token_authenticatable, :confirmable,
+  # :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  def active_for_authentication?
+    # Uncomment the below debug statement to view the properties of the returned self model values.
+    # logger.debug self.to_yaml
+    super && self.ativo?
+  end
+end
