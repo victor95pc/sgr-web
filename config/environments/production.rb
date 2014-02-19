fbff2b704e0d775ade4a6fec50133d01fa5f56af
Victor <vcastro@itquality.com>
2014-02-19 15:30:05 -0300

    Implementado algumas configurações para testar o ambiente de produção

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,4 +78,19 @@ SGR::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+
+  # Don't care if the mailer can't send.
+  config.action_mailer.raise_delivery_errors = false
+
+  # Print deprecation notices to the Rails logger.
+  config.active_support.deprecation = :log
+
+  # Raise an error on page load if there are pending migrations
+  config.active_record.migration_error = :page_load
+
+  # Debug mode disables concatenation and preprocessing of assets.
+  # This option may cause significant delays in view rendering with a large
+  # number of complex assets.
+  config.assets.debug = true
 end
