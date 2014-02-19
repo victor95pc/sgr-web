92007442e4f685a081b37d55409523b911fe7a7d
Victor <vcastro@itquality.com>
2014-02-19 15:46:02 -0300

    Alteração no carregamento do locale

--- config/application.rb
+++ config/application.rb
@@ -27,11 +27,10 @@ module SGR
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     config.time_zone = 'Brasilia'
 
-    config.i18n.enforce_available_locales = true
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    config.i18n.default_locale = :'pt-BR'
+    config.i18n.available_locales = [:es, 'pt-BR']
 
   end
 end
